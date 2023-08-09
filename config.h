02dc62dfc044177453f2fa9908fabbf8c426cd88
ChrisTitusTech <dfm.titus@gmail.com>
2023-08-09 09:25:54 -0500

    formatting fix

--- config.h
+++ config.h
@@ -53,7 +53,7 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-  	{ "steam",   NULL,     NULL,           1 << 8,    0,          0,           0,	     -1 },
+  { "steam",   NULL,     NULL,           1 << 8,    0,          0,           0,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "kitty",   NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
