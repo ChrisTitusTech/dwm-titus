143a295c62fb327d66d4fba8c0177949a81d932c
Chris Titus <contact@christitus.com>
2024-03-19 21:26:29 -0500

    fix systray icons
    20px size
    centered vertically

--- dwm.c
+++ dwm.c
@@ -2875,23 +2875,11 @@ void
 updatesystrayicongeom(Client *i, int w, int h)
 {
 	if (i) {
-		i->h = bh;
-		if (w == h)
-			i->w = bh;
-		else if (h == bh)
-			i->w = w;
-		else
-			i->w = (int) ((float)bh * ((float)w / (float)h));
-		applysizehints(i, &(i->x), &(i->y), &(i->w), &(i->h), False);
-		/* force icons into the systray dimensions if they don't want to */
-		if (i->h > bh) {
-			if (i->w == i->h)
-				i->w = bh;
-			else
-				i->w = (int) ((float)bh * ((float)i->w / (float)i->h));
-			i->h = bh;
-		}
-	}
+        i->w = w = 20; // Set icon width to 16
+        i->h = h = 20; // Set icon height to 16
+		i->y = ((bh - h) / 2); // Calculate y to center icon vertically
+        XMoveResizeWindow(dpy, i->win, i->x, i->y, i->w, i->h);
+    }
 }
 
 void
@@ -2932,6 +2920,7 @@ updatesystray(void)
 	unsigned int x = m->mx + m->mw;
 	unsigned int sw = TEXTW(stext) - lrpad + systrayspacing;
 	unsigned int w = 1;
+	unsigned int y = ((bh - 20) / 2);
 
 	if (!showsystray)
 		return;
@@ -2969,7 +2958,8 @@ updatesystray(void)
 		XMapRaised(dpy, i->win);
 		w += systrayspacing;
 		i->x = w;
-		XMoveResizeWindow(dpy, i->win, i->x, 0, i->w, i->h);
+		i->y = y;
+		XMoveResizeWindow(dpy, i->win, i->x, i->y, i->w, i->h);
 		w += i->w;
 		if (i->mon != m)
 			i->mon = m;
