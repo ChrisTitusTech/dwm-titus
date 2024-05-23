60070c280a0566a873a1bb7434c63bd52661cd7b
Tifeko <106326856+Tifeko@users.noreply.github.com>
2024-05-23 16:56:32 +0200

    changed icon
    changed mail icon to gitkraken

--- config.h
+++ config.h
@@ -46,7 +46,7 @@ static const char *const autostart[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "󰙯", "", "", "", "󰇮", "󰝚", "󰵆" };
+static const char *tags[] = { "", "", "󰙯", "", "", "", "󰇮", "󰝚", "󰵆" };
 
 static const char ptagf[] = "[%s %s]";	/* format of a tag label */
 static const char etagf[] = "[%s]";	/* format of an empty tag */
