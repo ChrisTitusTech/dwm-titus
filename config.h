c4036b3d587f8e4bd7e342872dc4af428656c4aa
Diego Pertierra <dpertierra@hotmail.com>
2024-07-20 01:35:57 +0200

    Changing keybinds and adding patches

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int refresh_rate    = 60;     /* matches dwm's mouse event processing to your monitor's refresh rate for smoother window interactions */
+static const unsigned int refresh_rate    = 144;     /* matches dwm's mouse event processing to your monitor's refresh rate for smoother window interactions */
 static const unsigned int enable_noborder = 1;      /* toggles noborder feature (0=disabled, 1=enabled) */
 static const unsigned int borderpx        = 1;      /* border pixel of windows */
 static const unsigned int snap            = 26;     /* snap pixel */
@@ -34,16 +34,17 @@ static const char *const autostart[] = {
   "xset", "s", "off", NULL,
   "xset", "s", "noblank", NULL,
   "xset", "-dpms", NULL,
+  "arandr", "$HOME/.screenlayout/default.sh", NULL,
   "dbus-update-activation-environment", "--systemd", "--all", NULL,
   "/usr/lib/polkit-kde-authentication-agent-1", NULL,
   "flameshot", NULL,
-  "copyq --start-server", NULL,
+  "copyq", "--start-server", NULL,
   "dunst", NULL,
   "picom", "--animations", "-b", NULL,
   "sh", "-c", "feh --randomize --bg-fill /media/LinuxData/Wallpapers/*", NULL,
   "nm-applet", NULL,
   "megasync", NULL,
-  "flatpak run dev.vencord.Vesktop", NULL,
+  "flatpak", "run", "dev.vencord.Vesktop", NULL,
   // "synergy", NULL,
   // "slstatus", NULL,
   NULL /* terminate */
@@ -53,7 +54,7 @@ static const char *const autostart[] = {
 static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const char ptagf[] = "[%s %s]";  /* format of a tag label */
-static const char etagf[] = "[%s]";     /* format of an empty tag */
+static const char etagf[] = "[%s %s]";  /* format of an empty tag */
 static const int lcaselbl = 0;          /* 1 means make tag label lowercase */
 
 static const Rule rules[] = {
@@ -93,7 +94,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 #define STATUSBAR "dwmblocks"
 /* commands */
-static const char *launchercmd[] = { "rofi", "-show", "drun", NULL };
+static const char *launchercmd[] = { "$HOME/.local/bin/run-rofi.sh", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
@@ -102,11 +103,11 @@ static Key keys[] = {
 	{ MODKEY|ControlMask,           XK_r,          spawn,                  SHCMD ("protonrestart")}, // restart protonvpn
 	{ MODKEY,                       XK_t,          spawn,                  {.v = termcmd } }, // spawn a terminal
 	{ MODKEY,                       XK_b,          spawn,                  SHCMD ("xdg-open https://")}, // open default browser
-	{ MODKEY,                       XK_p,          spawn,                  SHCMD ("flameshot full -p /media/drive/Screenshots/")}, // capture full screen screenshot
-	{ MODKEY|ShiftMask,             XK_p,          spawn,                  SHCMD ("flameshot gui -p /media/drive/Screenshots/")}, // open flameshot gui for screenshot selection
-	{ 0,           XK_Print,          spawn,                  SHCMD ("flameshot gui --clipboard")}, // copy screenshot to clipboard
+	// { MODKEY,                       XK_p,          spawn,                  SHCMD ("flameshot full -p /media/drive/Screenshots/")}, // capture full screen screenshot
+	// { MODKEY|ShiftMask,             XK_p,          spawn,                  SHCMD ("flameshot gui -p /media/drive/Screenshots/")}, // open flameshot gui for screenshot selection
+	{ 0,           XK_Print,          spawn,                  SHCMD ("flameshot gui")}, // copy screenshot to clipboard
 	{ MODKEY,                       XK_e,          spawn,                  SHCMD ("thunar")}, // open thunar file manager
-	{ MODKEY,                       XK_w,          spawn,                  SHCMD ("looking-glass-client -F")}, // start Looking glass
+	// { MODKEY,                       XK_w,          spawn,                  SHCMD ("looking-glass-client -F")}, // start Looking glass
 	{ 0,                            0x1008ff02,    spawn,                  SHCMD ("xbacklight -inc 10")}, // increase backlight brightness
 	{ 0,                            0x1008ff03,    spawn,                  SHCMD ("xbacklight -dec 10")}, // decrease backlight brightness
 	{ 0,                            0x1008ff1b,    spawn,                  SHCMD ("xbacklight -inc 10")}, // increase backlight brightness
@@ -117,6 +118,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_b,          togglebar,              {0} }, // toggle bar visibility
 	{ MODKEY,                       XK_j,          focusstack,             {.i = +1 } }, // focus on the next client in the stack
 	{ MODKEY,                       XK_k,          focusstack,             {.i = -1 } }, // focus on the previous client in the stack
+	{ MODKEY,                       XK_Right,          focusstack,             {.i = +1 } }, // focus on the next client in the stack
+	{ MODKEY,                       XK_Left,          focusstack,             {.i = -1 } }, // focus on the previous client in the stack
 	{ MODKEY|ShiftMask,             XK_j,          movestack,              {.i = +1 } }, // move stack up
 	{ MODKEY|ShiftMask,             XK_k,          movestack,              {.i = -1 } }, // move stack down
 	{ MODKEY,                       XK_i,          incnmaster,             {.i = +1 } }, // decrease the number of clients in the master area
@@ -125,6 +128,11 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,          setmfact,               {.f = +0.05} }, // increase the size of the master area compared to the stack area(s)
 	{ MODKEY|ShiftMask,             XK_h,          setcfact,               {.f = +0.25} }, // increase size respective to other windows within the same area
 	{ MODKEY|ShiftMask,             XK_l,          setcfact,               {.f = -0.25} }, // decrease client size respective to other windows within the same area
+	{ MODKEY|ControlMask,                       XK_Left,          setmfact,               {.f = -0.05} }, // decrease the size of the master area compared to the stack area(s)
+	{ MODKEY|ControlMask,                       XK_Right,          setmfact,               {.f = +0.05} }, // increase the size of the master area compared to the stack area(s)
+	{ MODKEY|ShiftMask,             XK_Left,          setcfact,               {.f = +0.25} }, // increase size respective to other windows within the same area
+	{ MODKEY|ShiftMask,             XK_Right,          setcfact,               {.f = -0.25} }, // decrease client size respective to other windows within the same area
+
 	{ MODKEY|ShiftMask,             XK_o,          setcfact,               {.f =  0.00} }, // reset client area
 	{ MODKEY,                       XK_Return,     zoom,                   {0} }, // moves the currently focused window to/from the master area (for tiled layouts)
 	{ MODKEY,                       XK_Tab,        view,                   {0} }, // view last focused tag
@@ -137,16 +145,18 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_y,          togglefakefullscreen,   {0} }, // toggles "fake" fullscreen for the selected window
 	{ MODKEY,                       XK_0,          view,                   {.ui = ~0 } }, // view all tags on the current monitor
 	{ MODKEY,                       XK_comma,      focusmon,               {.i = -1 } }, // focus on the previous monitor, if any
-	{ MODKEY,                       XK_period,     focusmon,               {.i = +1 } }, // focus on the next monitor, if any
-	{ MODKEY|ShiftMask,             XK_comma,      tagmon,                 {.i = -1 } }, // tag previous monitor
-	{ MODKEY|ShiftMask,             XK_period,     tagmon,                 {.i = +1 } }, // tag next monitor
+	{ MODKEY,                       XK_period,     focusmon,               {.i = +1 } }, // focus on the next monitor,  if any
+	{ MODKEY|ShiftMask,             XK_comma,      tagmon,                {.i = -1 } }, // tag previous monitor
+	{ MODKEY|ShiftMask,             XK_period,     tagmon,                {.i = +1 } }, // tag next monitor
+	{ MODKEY|ShiftMask,             XK_Left,       tagmon,                {.i = -1 } }, // tag previous monitor
+	{ MODKEY|ShiftMask,             XK_Right,      tagmon,                {.i = +1 } }, // tag next monitor
 	TAGKEYS(                        XK_1,                                  0)
 	TAGKEYS(                        XK_2,                                  1)
 	TAGKEYS(                        XK_3,                                  2)
 	TAGKEYS(                        XK_4,                                  3)
 	TAGKEYS(                        XK_5,                                  4)
 	{ MODKEY|ShiftMask,             XK_q,          quit,                   {0} }, // exit dwm
-	{ MODKEY|ControlMask,           XK_q,          spawn,                  SHCMD("$HOME/.config/rofi/powermenu.sh")}, // exit dwm
+	{ MODKEY|ControlMask,           XK_q,          spawn,                  SHCMD("$HOME/.local/bin/rofi-power-menu.sh")}, // exit dwm
 	// { MODKEY|ControlMask|ShiftMask, XK_r,          spawn,                  SHCMD("systemctl reboot")}, // reboot system
 	// { MODKEY|ControlMask|ShiftMask, XK_s,          spawn,                  SHCMD("systemctl suspend")}, // suspend system
 };
