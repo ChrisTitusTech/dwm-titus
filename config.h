be5ba2caadd33cd436e0e3525db8cdafb58193dc
Jere Laitinen <85607228+jere-a@users.noreply.github.com>
2024-07-22 15:17:05 +0300

    Added terminator and fixed formatting.

--- config.h
+++ config.h
@@ -60,7 +60,8 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "kitty",   NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ "alacritty",   NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "alacritty",   NULL,     NULL,       0,         0,          1,           0,        -1 },
+	{ "terminator",	 NULL,     NULL,	   0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
