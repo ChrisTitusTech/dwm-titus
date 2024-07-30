e6f2f01f2aebe9a4c45cf005b3e1d1705e20a2cf
Chris Titus <contact@christitus.com>
2024-07-30 17:19:45 -0500

    minor fixes

--- config.h
+++ config.h
@@ -87,24 +87,23 @@ static const Layout layouts[] = {
 
 /* Helper macros for spawning commands */
 #define SHCMD(cmd) { .v = (const char*[]){ NULL, "/bin/sh", "-c", cmd, NULL } }
-#define CMD(...)   { .v = (const char*[]){ NULL, __VA_ARGS__, NULL } }
-
 #define STATUSBAR "dwmblocks"
 
 /* commands */
 static const char *launchercmd[] = { "rofi", "-show", "drun", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
 	/* modifier                     key            function                argument */
 	{ MODKEY,                       XK_r,          spawn,                  {.v = launchercmd} }, // spawn rofi for launching other programs
 	{ MODKEY|ControlMask,           XK_r,          spawn,                  SHCMD ("protonrestart")}, // restart protonvpn
-	{ MODKEY,                       XK_x,          spawn,                  CMD("alacritty") }, // spawn a terminal
-	{ MODKEY,                       XK_b,          spawn,                  CMD ("xdg-open https://")}, // open default browser
+	{ MODKEY,                       XK_x,          spawn,                  {.v = termcmd } }, // spawn a terminal
+	{ MODKEY,                       XK_b,          spawn,                  SHCMD ("xdg-open https://")}, // open default browser
 	{ MODKEY,                       XK_p,          spawn,                  SHCMD ("flameshot full -p /media/drive/Screenshots/")}, // capture full screen screenshot
 	{ MODKEY|ShiftMask,             XK_p,          spawn,                  SHCMD ("flameshot gui -p /media/drive/Screenshots/")}, // open flameshot gui for screenshot selection
 	{ MODKEY|ControlMask,           XK_p,          spawn,                  SHCMD ("flameshot gui --clipboard")}, // copy screenshot to clipboard
-	{ MODKEY,                       XK_e,          spawn,                  CMD ("thunar")}, // open thunar file manager
-	{ MODKEY,                       XK_w,          spawn,                  CMD ("looking-glass-client -F")}, // start Looking glass
+	{ MODKEY,                       XK_e,          spawn,                  SHCMD ("thunar")}, // open thunar file manager
+	{ MODKEY,                       XK_w,          spawn,                  SHCMD ("looking-glass-client -F")}, // start Looking glass
 	{ 0,                            0x1008ff02,    spawn,                  SHCMD ("xbacklight -inc 10")}, // increase backlight brightness
 	{ 0,                            0x1008ff03,    spawn,                  SHCMD ("xbacklight -dec 10")}, // decrease backlight brightness
 	{ 0,                            0x1008ff1b,    spawn,                  SHCMD ("xbacklight -inc 10")}, // increase backlight brightness
