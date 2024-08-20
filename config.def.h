e70d1e35027d32429a6c0c15876438a0722a954b
ayako <ayako.gfx@gmail.com>
2024-08-21 02:44:57 +0530

    FixSwallow Patch

--- config.def.h
+++ config.def.h
@@ -50,6 +50,7 @@ static const Rule rules[] = {
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+  { "Alacritty",NULL,    NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
