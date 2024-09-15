b8e5399caed65b1ee8878e7ec45b240b5ff36419
Akkhil Coding Hub <akkhilsharmaclass@gmail.com>
2024-09-15 12:15:20 +0530

    fixing xfce and adding keybindings for laptop

--- config.h
+++ config.h
@@ -38,7 +38,7 @@ static const char *const autostart[] = {
     "/usr/lib/mate-polkit/polkit-mate-authentication-agent-1", NULL,
     "flameshot", NULL,
     "dunst", NULL,
-    "xfce4-power-manager ","--daemon", NULL,
+    "xfce4-power-manager", "--daemon", NULL,
     "picom","--animations", "-b", NULL,
     "sh", "-c", "feh --randomize --bg-fill ~/Pictures/backgrounds/*", NULL,
     "synergy", NULL,
@@ -104,6 +104,13 @@ static Key keys[] = {
 	{ MODKEY|ControlMask,           XK_p,          spawn,                  SHCMD ("flameshot gui --clipboard")}, // copy screenshot to clipboard
 	{ MODKEY,                       XK_e,          spawn,                  SHCMD ("thunar")}, // open thunar file manager
 	{ MODKEY,                       XK_w,          spawn,                  SHCMD ("looking-glass-client -F")}, // start Looking glass
+	/* Laptop controls */
+	{ 0,                            XF86XK_MonBrightnessUp,   spawn,        SHCMD ("brillo -u 300000 -A 10")}, // increase backlight brightness
+	{ 0,                            XF86XK_MonBrightnessDown, spawn,        SHCMD ("brillo -u 300000 -U 10")}, // decrease backlight brightness
+	{ 0,                            XF86XK_AudioMute,         spawn,        SHCMD ("amixer sset Master $(amixer get Master | grep -q '\\[on\\]' && echo 'mute' || echo 'unmute')")}, // toggle mute/unmute
+	{ 0,                            XF86XK_AudioLowerVolume,  spawn,        SHCMD ("amixer sset Master 5%- unmute")}, // decrease volume
+	{ 0,                            XF86XK_AudioRaiseVolume,  spawn,        SHCMD ("amixer sset Master 5%+ unmute")}, // increase volume
+	/* PC controls */
 	{ 0,                            XK_F7,         spawn,                  SHCMD ("brillo -u 300000 -A 10")}, // increase backlight brightness
 	{ 0,                            XK_F6,         spawn,                  SHCMD ("brillo -u 300000 -U 10")}, // decrease backlight brightness
 	{ 0,                            XK_F1,         spawn,                  SHCMD ("amixer sset Master $(amixer get Master | grep -q '\\[on\\]' && echo 'mute' || echo 'unmute')")}, // toggle mute/unmute
