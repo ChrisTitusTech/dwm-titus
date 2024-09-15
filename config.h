539ea78eb5d2a5692c1835a39973a7b38662371e
Akkhil Coding Hub <113294188+AkkhilCodingHub@users.noreply.github.com>
2024-09-15 12:08:08 +0530

    Small fixes

--- config.h
+++ config.h
@@ -38,7 +38,7 @@ static const char *const autostart[] = {
     "/usr/lib/mate-polkit/polkit-mate-authentication-agent-1", NULL,
     "flameshot", NULL,
     "dunst", NULL,
-    "picom", "-b", NULL,
+    "picom", "--animations", "-b", NULL,
     "sh", "-c", "feh --randomize --bg-fill ~/Pictures/backgrounds/*", NULL,
     "synergy", NULL,
     "slstatus", NULL,
@@ -103,11 +103,11 @@ static Key keys[] = {
 	{ MODKEY|ControlMask,           XK_p,          spawn,                  SHCMD ("flameshot gui --clipboard")}, // copy screenshot to clipboard
 	{ MODKEY,                       XK_e,          spawn,                  SHCMD ("thunar")}, // open thunar file manager
 	{ MODKEY,                       XK_w,          spawn,                  SHCMD ("looking-glass-client -F")}, // start Looking glass
-	{ 0,                            XK_F7,         spawn,                  SHCMD ("brillo -u 300000 -A 10")}, // increase backlight brightness
-	{ 0,                            XK_F6,         spawn,                  SHCMD ("brillo -u 300000 -U 10")}, // decrease backlight brightness
-	{ 0,                            XK_F1,         spawn,                  SHCMD ("amixer sset Master $(amixer get Master | grep -q '\\[on\\]' && echo 'mute' || echo 'unmute')")}, // toggle mute/unmute
-	{ 0,                            XK_F2,         spawn,                  SHCMD ("amixer sset Master 5%- unmute")}, // decrease volume
-	{ 0,                            XK_F3,         spawn,                  SHCMD ("amixer sset Master 5%+ unmute")}, // increase volume
+ 	{ 0,                            XF86XK_MonBrightnessUp,    spawn,          SHCMD ("xbacklight -inc 10")},
+    	{ 0,                            XF86XK_MonBrightnessDown,  spawn,          SHCMD ("xbacklight -dec 10")},
+    	{ 0,                            XF86XK_AudioLowerVolume,   spawn,          SHCMD ("amixer sset Master 5%- unmute")},
+    	{ 0,                            XF86XK_AudioMute,          spawn,          SHCMD ("amixer sset Master $(amixer get Master | grep -q '\\[on\\]' && echo 'mute' || echo 'unmute')")},
+    	{ 0,                            XF86XK_AudioRaiseVolume,   spawn,          SHCMD ("amixer sset Master 5%+ unmute")},
 	{ MODKEY|ShiftMask,             XK_b,          togglebar,              {0} }, // toggle bar visibility
 	{ MODKEY,                       XK_j,          focusstack,             {.i = +1 } }, // focus on the next client in the stack
 	{ MODKEY,                       XK_k,          focusstack,             {.i = -1 } }, // focus on the previous client in the stack
