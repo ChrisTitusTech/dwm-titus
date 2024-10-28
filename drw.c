0dbe8d2221f3675dd989fb9b9b33a1ca4c111212
Raymond Cole <rc@wolog.xyz>
2024-10-28 00:34:55 +0000

    Avoid unsigned integer underflow in drw_text()

--- drw.c
+++ drw.c
@@ -315,6 +315,8 @@ drw_text(Drw *drw, int x, int y, unsigned int w, unsigned int h, unsigned int lp
 	} else {
 		XSetForeground(drw->dpy, drw->gc, drw->scheme[invert ? ColFg : ColBg].pixel);
 		XFillRectangle(drw->dpy, drw->drawable, drw->gc, x, y, w, h);
+		if (w < lpad)
+			return x + w;
 		d = XftDrawCreate(drw->dpy, drw->drawable,
 		                  DefaultVisual(drw->dpy, drw->screen),
 		                  DefaultColormap(drw->dpy, drw->screen));
