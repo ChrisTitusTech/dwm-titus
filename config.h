e939d26684f18fa62090e2c65fdb877b853fea24
Chris Titus <contact@christitus.com>
2024-11-13 12:27:03 -0600

    Update config.h

--- config.h
+++ config.h
@@ -64,7 +64,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.75; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.6; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
