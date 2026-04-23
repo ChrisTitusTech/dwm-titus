af5315d5548f79e2d363e3799a56f9640a2efa03
titus <contact@christitus.com>
2026-04-22 22:03:06 -0500

    fix font name formatting for consistency

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const int topbar                       = 1;   /* 0 means bottom bar */
 #define ICONSIZE                              17     /* icon size */
 #define ICONSPACING                           5      /* space between icon and title */
 #define SHOWWINICON                           1      /* 0 means no winicon */
-static const char *fonts[]                    = { "MesloLGS Nerd Font Mono:size=14:antialias=true:autohint=true:hintstyle=hintfull", "NotoColorEmoji:pixelsize=16:antialias=true:autohint=true" };
+static const char *fonts[]                    = { "MesloLGS NF:size=14:antialias=true:autohint=true:hintstyle=hintfull", "Noto Color Emoji:pixelsize=16:antialias=true:autohint=true" };
 
 
 
